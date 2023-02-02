2cf9b65c0e0affa0a7468102d556b76a14b1e581
kaizerenrique <kayserenrique@gmail.com>
2023-02-02 15:21:43 -0400

    categoria listar y buscar

--- app/Http/Livewire/Admin/Categoriacomponente.php
+++ app/Http/Livewire/Admin/Categoriacomponente.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Categoria;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Categoriacomponente extends Component
 {
+    use WithPagination;
+
+    //barra de busqueda
+    public $buscar;
+
+    protected $queryString = [
+        'buscar' => ['except' => '']
+    ];
+
     public function render()
     {
-        return view('livewire.admin.categoriacomponente');
+        $categorias = Categoria::where('categoria', 'like', '%'.$this->buscar . '%')  //buscar por nombre de categoria 
+                                ->orderBy('id','desc') //ordenar de forma decendente
+                                ->paginate(10); //paginacion
+        
+        return view('livewire.admin.categoriacomponente',[
+            'categorias' => $categorias
+        ]);
+    }
+
+    //Actualizar tabla para corregir falla de busqueda
+    public function updatingBuscar()
+    {
+        $this->resetPage();
     }
 }
