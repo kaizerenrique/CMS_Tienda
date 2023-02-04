821bcc617b5cf7de2a43b2e40da3c8770c38d677
kaizerenrique <kayserenrique@gmail.com>
2023-02-04 12:04:30 -0400

    cientes componentes

---
+++ app/Http/Livewire/Admin/Clientescomponente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class Clientescomponente extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.clientescomponente');
+    }
+}
