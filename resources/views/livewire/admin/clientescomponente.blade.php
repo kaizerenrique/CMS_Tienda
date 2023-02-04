937e04f31fbac2a2de2ca0a274971e8d37a90795
marianabolet <marianaboletmartinez@gmail.com>
2023-02-04 14:52:53 -0400

    Estadistica

--- resources/views/livewire/admin/clientescomponente.blade.php
+++ resources/views/livewire/admin/clientescomponente.blade.php
@@ -1,3 +1,97 @@
 <div class="p-6">
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<!--Seccion de Busqueda Cliente-->
+    <tbody class="text-gray-600 text-sm font-light">
+        <div class="flex flex-wrap items-center px-4 py-2">
+            <div class="relative w-full max-w-full flex-grow flex-1">
+              <h3 class="font-semibold text-lg text-gray-800 ">Listado de Clientes </h3>
+            </div>
+            <div class="flex flex-col items-center w-full max-w-xl">
+              <input wire:model="buscar" type="search" placeholder="Buscar"
+                class="input input-bordered w-full max-w-xs rounded-lg" />
+            </div>
+            <div class="relative w-full max-w-full flex-grow flex-1 text-right">
+              <button type="button" wire:click="agregarcategoria"
+                class="border border-blue-700 bg-blue-700 text-white rounded-lg px-4 py-2 transition duration-500 ease select-none hover:bg-blue-800 focus:outline-none focus:shadow-outline">
+                <div class="flex items-center">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                      </svg>
+                  </span>
+                  <span class="ml-2">Cliente</span>
+                </div>        
+              </button>
+            </div>
+          </div>        
+    </tbody> 
+<!--Tabla Usuario-->
+ <div class="overflow-x-auto">
+    <div class="bg-white shadow-md rounded my-6">
+      <table class="min-w-max w-full table-auto">
+        <thead>
+          <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+            <th class="py-3 px-6 text-left">Nombre</th>
+            <th class="py-3 px-6 text-left">Email </th>
+            <th class="py-3 px-6 text-center">Telefono</th>
+            <th class="py-3 px-6 text-center">Acciones</th>
+          </tr>
+        </thead>
+    {{-- <!-- @foreach ($categorias as $categoria)-->
+      <tr class="border-b border-gray-200 hover:bg-gray-100">
+        <td class="py-3 px-6 text-left whitespace-nowrap">
+     {{ $categoria->categoria }}
+        </td>
+        <td class="py-3 px-6 text-left">
+          @switch($categoria->stado)
+            @case(0)
+            <span
+              class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-lg">Inactivo</span>
+            @break
+            @case(1)
+            <span
+              class="bg-emerald-100 text-emerald-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-lg">Active</span>
+            @break
+            @default
+          @endswitch
+
+        </td>
+        <td class="py-3 px-6 text-center">
+          {{ $categoria->productos->count() }}
+        </td>
+        <td class="py-3 px-6 text-center">
+          <div class="flex item-center justify-center">
+            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+              </svg>
+            </div>
+            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+              </svg>
+            </div>
+            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+              </svg>
+            </div>
+          </div>
+        </td>
+
+      </tr>
+
+      @endforeach
+    </tbody>
+  </table>
+</div>
+<div class="mt-4">
+  <!--  {{ $categorias->links() }}-->--}} 
+</div>
+</div>
+
 </div>
--- resources/views/livewire/admin/estadisticascomponente.blade.php
+++ resources/views/livewire/admin/estadisticascomponente.blade.php
@@ -1,3 +1,59 @@
 <div class="p-6">
-    {{-- Be like water. --}}
+      <!-- Seccion que contiene Barra de estadistica--> 
+        <h5 class="font-semibold text-lg text-gray-800 ">Barra de Estadistica</h5>
+        <div class="mt-10 mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+            <a href="#">
+              <div class="mt-1 p-2">
+                <h2 class="text-slate-700">Descripcion</h2>
+                <p class="mt-1 text-sm text-slate-400">Detalles</p>
+                <div class="mt-3 flex items-end justify-between">
+                  <p>
+                    <span class="text-lg font-bold text-blue-500">$450</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </article>
+          <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+            <a href="#">
+              <div class="mt-1 p-2">
+                <h2 class="text-slate-700">Descripcion </h2>
+                <p class="mt-1 text-sm text-slate-400">Detalles </p>
+                <div class="mt-3 flex items-end justify-between">
+                  <p>
+                    <span class="text-lg font-bold text-blue-500">$450</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </article>
+          <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+            <a href="#">
+              <div class="mt-1 p-2">
+                <h2 class="text-slate-700">Descripcion</h2>
+                <p class="mt-1 text-sm text-slate-400">Detalles</p>
+                <div class="mt-3 flex items-end justify-between">
+                  <p>
+                    <span class="text-lg font-bold text-blue-500">$450</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </article>
+          <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+            <a href="#">
+              <div class="mt-1 p-2">
+                <h2 class="text-slate-700">Descripcion</h2>
+                <p class="mt-1 text-sm text-slate-400">Detalles</p>
+                <div class="mt-3 flex items-end justify-between">
+                  <p>
+                    <span class="text-lg font-bold text-blue-500">$450</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </article>
+        </div>
+
 </div>
