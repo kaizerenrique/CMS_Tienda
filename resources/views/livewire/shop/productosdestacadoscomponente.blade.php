4aa08c00626a0cb430508a7c35cb0c5184b1a790
kaizerenrique <kayserenrique@gmail.com>
2023-02-07 16:39:36 -0400

    Corregir error en el botón de agregar al carro

--- resources/views/livewire/shop/productosdestacadoscomponente.blade.php
+++ resources/views/livewire/shop/productosdestacadoscomponente.blade.php
@@ -12,16 +12,28 @@
                     <span class="text-lg font-bold text-blue-500">{{ number_format($producto->costo , 2) }}</span>
                     <span class="text-sm text-slate-400">{{ $producto->metodo }}</span>
                 </p>
-                <div
-                    class="flex items-center space-x-1.5 rounded-lg bg-blue-700 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="h-4 w-4">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                    </svg>
-                    <button class="text-sm" wire:click="agregaralcarro({{ $producto->id }})">Agregar</button>
-                </div>
-                </div>
+                
+
+                @auth
+                    <div class="flex items-center space-x-1.5 rounded-lg bg-blue-700 px-4 py-1.5
+                    text-white duration-100 hover:bg-blue-600" wire:click="agregaralcarro({{ $producto->id }})">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="h-4 w-4">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        </svg>
+                        
+                        <button class="text-sm">Agregar</button>
+                    </div>  
+                        
+                    @else                    
+                        <a href="{{ route('login') }}">
+                            <button type="button" class="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium 
+                            text-blue-700 focus:outline-none transition duration-500 focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg">
+                                Login
+                            </button>                        
+                        </a>
+                @endauth
             </div>            
         </article>        
     @endforeach 
