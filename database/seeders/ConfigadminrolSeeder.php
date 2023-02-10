20eb062a02ec7b95e725bd28181ee3209c2f98ac
kaizerenrique <kayserenrique@gmail.com>
2023-02-10 13:41:08 -0400

    roles y permisos

--- database/seeders/ConfigadminrolSeeder.php
+++ database/seeders/ConfigadminrolSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use App\Models\User;
 use App\Models\Banco;
 use File;
@@ -17,24 +19,39 @@ class ConfigadminrolSeeder extends Seeder
      */
     public function run()
     {
+        //roles del sistema
+        $admin = Role::create(['name' => 'Administrador']); //Administrador del Sistema
+        $user = Role::create(['name' => 'Usuario']); //Usuario
+        $clie = Role::create(['name' => 'Cliente']); //Cliente
+
+        //permisos del menu
+        Permission::create(['name' => 'menuCategoria'])->syncRoles([$admin]);
+        Permission::create(['name' => 'menuEstadistica'])->syncRoles([$admin]);
+        Permission::create(['name' => 'menuConfiguraciones'])->syncRoles([$admin]);
+
+        Permission::create(['name' => 'menuProducto'])->syncRoles([$admin, $user]);
+        Permission::create(['name' => 'menuCliente'])->syncRoles([$admin, $user]);
+
+        Permission::create(['name' => 'menuPanel'])->syncRoles([$admin, $user, $clie]); 
+
         //usuario
         User::create([
             'name' => 'Oliver Gomez',
             'email' => 'kayserenrique@gmail.com',
             'password' => bcrypt('123456789'),
-        ]);
+        ])->assignRole('Administrador');
 
         User::create([
             'name' => 'Marina Bolet',
             'email' => 'marianaboletmartinez@gmail.com',
             'password' => bcrypt('123456789'),
-        ]);
+        ])->assignRole('Administrador');
 
         User::create([
             'name' => 'Dayana Martinez',
             'email' => 'dmaro006@gmail.com',
             'password' => bcrypt('123456789'),
-        ]);
+        ])->assignRole('Administrador');
 
         $json = File::get("database/data/bancos.json");
         $data = json_decode($json);
