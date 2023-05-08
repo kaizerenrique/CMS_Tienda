69cf7f0decc7a26a1f60c8cf32461cd9db163782
Oliver Gomez <kayserenrique@gmail.com>
2023-05-08 18:46:58 -0400

    navegacion

--- resources/views/livewire/comp/navbarcomponente.blade.php
+++ resources/views/livewire/comp/navbarcomponente.blade.php
@@ -8,42 +8,6 @@
         <span class="self-center whitespace-nowrap text-xl font-semibold">Tienda</span>
     </a>
     <div class="mt-2 sm:mt-0 sm:flex md:order-2">
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <!--componente indicardo de carro-->
-            <livewire:shop.cartcomponente />
-        </div>
-        @if (Route::has('login'))
-        <div class="mt-2 sm:mt-0 sm:flex md:order-2">
-            @auth
-            <a href="{{ url('/dashboard') }}">
-                <button type="button"
-                    class="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium 
-                          text-blue-700 focus:outline-none transition duration-500 focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg">
-                    Dashboard
-                </button>
-            </a>
-            @else
-
-            <a href="{{ route('login') }}">
-                <button type="button"
-                    class="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium 
-                          text-blue-700 focus:outline-none transition duration-500 focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg">
-                    Login
-                </button>
-            </a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">
-                <button type="button"
-                    class="rounde mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium
-                              text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg">
-                    Register
-                </button>
-            </a>
-            @endif
-            @endauth
-        </div>
-        @endif
         <button data-collapse-toggle="navbar-sticky" type="button"
             class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
             aria-controls="navbar-sticky" aria-expanded="false">
@@ -82,7 +46,40 @@
                     class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
                     Contacto
                 </a>
-            </li>                  
+            </li>             
+                @if (Route::has('login'))
+                    
+                        @auth
+                        <li>
+                            <a href="{{ url('/dashboard') }}" 
+                            class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
+                                Dashboard                                
+                            </a>
+                        </li> 
+                        <li>
+                            <!--componente indicardo de carro-->
+                            <livewire:shop.cartcomponente />
+                        </li>                      
+                        
+                        @else
+                        <li>
+                            <a href="{{ route('login') }}"
+                            class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">                                
+                                Login                                
+                            </a>
+                        </li>                        
+
+                        @if (Route::has('register'))
+                            <li>
+                                <a href="{{ route('register') }}"
+                                class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
+                                    Register
+                                </a>
+                            </li>
+                        @endif
+                        @endauth                    
+                @endif
+            
             
         </ul>
     </div>
